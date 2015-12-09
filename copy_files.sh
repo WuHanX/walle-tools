b536d0e0cde3340057ca12539859778a9d34fe69
Meiyou Chen <cmy@rock-chips.com>
2015-12-09 20:27:47 +0800

    Don't copy ffmpeg libs at there

--- copy_files.sh
+++ copy_files.sh
@@ -6,16 +6,9 @@ if [ "$1"x == ""x  ]; then
 fi
 
 WALLE_ROOT=$1
-ROOTFS=$WALLE_ROOT/output/rootfs
 IMAGE=$WALLE_ROOT/output/image
 
-rm -rf $ROOTFS
-mkdir -p $ROOTFS
-
 echo "copying.."
-mkdir -p $ROOTFS/root/
-cp -rf $WALLE_ROOT/hardware/rk_ffmpeg_linux/out/* $ROOTFS/
-cp -rf $WALLE_ROOT/hardware/rk_ffmpeg_linux/ffmpeg-example/ffmpegexample $ROOTFS/root/
 
 mkdir -p $IMAGE
 cp -rf $WALLE_ROOT/system/lubuntu/RK3288UbootLoader_*.bin $IMAGE
